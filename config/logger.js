92409d42a4f001dd91b789bde7294031ee9bea74
SiddharthMishra28 <connectwithsiddharthm@gmail.com>
2024-10-09 13:45:13 +0530

    modified logger

--- config/logger.js
+++ config/logger.js
@@ -1,39 +1,16 @@
-// config/logger.js
+const winston = require('winston');
+const { createLogger, transports, format } = winston;
 
-const { createLogger, format, transports } = require('winston');
-const path = require('path');
-
-// Create a custom format for logs
-const logFormat = format.combine(
-    format.timestamp({
-        format: 'YYYY-MM-DD HH:mm:ss'
-    }),
-    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-);
-
-// Define the logger configuration
 const logger = createLogger({
-    level: 'error', // Set logging level
-    format: logFormat,
-    transports: [
-        // Write logs to a file
-        new transports.File({ filename: path.join(__dirname, '../logs/error.log'), level: 'error' }),
-        // Console logging (optional)
-        new transports.Console({
-            format: format.combine(
-                format.colorize(),
-                format.simple()
-            )
-        })
-    ],
-    exceptionHandlers: [
-        // Handle uncaught exceptions and log them
-        new transports.File({ filename: path.join(__dirname, '../logs/exceptions.log') })
-    ],
-    rejectionHandlers: [
-        // Handle unhandled promise rejections
-        new transports.File({ filename: path.join(__dirname, '../logs/rejections.log') })
-    ]
+  level: 'info',
+  format: format.combine(
+    format.timestamp(),
+    format.json()
+  ),
+  transports: [
+    // Remove the file transport in serverless environments like Netlify
+    new transports.Console()  // Log to console
+  ]
 });
 
 module.exports = logger;
